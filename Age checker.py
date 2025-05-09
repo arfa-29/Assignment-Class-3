8c27aa2a3d6b0b9d3c28ac1f769c986513bc46d9
arfa-29 <arfaasif1@gmail.com>
2025-05-10 01:58:07 +0500

    Update Age checker.py

--- Age checker.py
+++ Age checker.py
@@ -2,31 +2,31 @@
 
 age = int(input("Enter your age: "))
 if age >= 14 and age <= 15:
-    print("You are elegible for admission in Class X")
+    print("You are eligible for admission in Class X")
 elif age >= 13 and age < 14:
-    print("You are elegible for admission in Class IX")
+    print("You are eligible for admission in Class IX")
 elif age >= 12 and age < 13:
-    print("You are elegible for admission in Class VIII")
+    print("You are eligible for admission in Class VIII")
 elif age >= 11 and age < 12:
-    print("You are elegible for admission in Class VII")
+    print("You are eligible for admission in Class VII")
 elif age >= 10 and age < 11:
-    print("You are elegible for admission in Class VI")
+    print("You are eligible for admission in Class VI")
 elif age >= 9 and age < 10:
-    print("You are elegible for admission in Class V")
+    print("You are eligible for admission in Class V")
 elif age >= 8 and age < 9:
-    print("You are elegible for admission in Class IV")
+    print("You are eligible for admission in Class IV")
 elif age >= 7 and age < 8:
-    print("You are elegible for admission in Class III")
+    print("You are eligible for admission in Class III")
 elif age >= 6 and age < 7:
-    print("You are elegible for admission in Class II")
+    print("You are eligible for admission in Class II")
 elif age >= 5 and age < 6:
-    print("You are elegible for admission in Class I")
+    print("You are eligible for admission in Class I")
 elif age >= 4 and age < 5:
-    print("You are elegible for admission in KG-II")
+    print("You are eligible for admission in KG-II")
 elif age >= 3 and age < 4:
-    print("You are elegible for admission in KG-I")
+    print("You are eligible for admission in KG-I")
 elif age >= 2 and age < 3:
-    print("You are elegible for admission in Montessori")
+    print("You are eligible for admission in Montessori")
 else:
-    print("You are not elegibe for admission")
+    print("You are not eligibe for admission")
 
